262b69f824d9a63461e80725d05c3c4bbb85a7ba
Filipe Carbonera de Souza <77364648+filipecarbonera@users.noreply.github.com>
2024-05-12 03:15:32 -0300

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain_openai import ChatOpenAI
